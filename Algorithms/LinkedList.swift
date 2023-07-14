aa9116a164f21c9acb46ebc48bd92df52955a0ec
mauzec <49997318+Mauzec@users.noreply.github.com>
2023-07-14 18:28:00 +0300

    update: linked list done

--- Algorithms/LinkedList.swift
+++ Algorithms/LinkedList.swift
@@ -37,7 +37,7 @@ public final class LinkedList<T> {
     // return: LL Node
     public func node(at index: Int) -> Node {
         assert(head != nil, "List is empty")
-        assert(index >= count, "index must be less to list size")
+        assert(index < count, "index must be less to list size")
         assert(index >= 0, "index must be greater or equal to 0")
         
         if index == 0 {
@@ -71,6 +71,7 @@ public final class LinkedList<T> {
         } else {
             head = new
         }
+        count += 1
     }
     
     // Append value to the end
@@ -106,8 +107,159 @@ public final class LinkedList<T> {
             new.next = head
             head?.previous = new
             head = new
+            count += 1
+        } else if index == count {
+            append(new)
         } else {
-            let prev = node(
+            let prev = node(at: index - 1)
+            let next = prev.next
+            new.previous = prev
+            new.next = next
+            next?.previous = new
+            prev.next = new
+            count += 1
         }
+    }
+    
+    // Remove node
+    // node: node to be deleted
+    // return: data value in the deleted node
+    @discardableResult public func remove(node: Node) -> T {
+        let prev = node.previous
+        let next = node.next
+        
+        if let next = next {
+            next.previous = prev
+        } else {
+            tail = prev
+        }
+        
+        if let prev = prev {
+            prev.next = next
+        } else {
+            head = next
+        }
+        
+        node.previous = nil
+        node.next = nil
+        return node.value
+    }
+    
+    public func removeAll() {
+        head = nil
+    }
+    
+    @discardableResult public func removeLast() -> T {
+        assert(!isEmpty, "list is empty")
+        return remove(node: tail!)
+    }
+    
+    @discardableResult public func remove(at index: Int) -> T {
+        let node = self.node(at: index)
+        return remove(node: node)
+    }
+    
+    // Reverse method
+    public func reverse() {
+        var node = head
+        while let curr = node {
+            node = curr.next
+            swap(&curr.next, &curr.previous)
+        }
+    }
+    // Map method
+    public func map<K>(transform: (T) -> K) -> LinkedList<K> {
+        let result = LinkedList<K>()
+        var node = head
+        while let curr = node {
+            result.append(transform(curr.value))
+            node = curr.next
+        }
+        return result
+    }
+    
+    // Filter method
+    public func filter(predicate: (T) -> Bool) -> LinkedList<T> {
+        let result = LinkedList<T>()
+        var node = head
+        while let curr = node {
+            if predicate(curr.value) {
+                result.append(curr.value)
+            }
+            node = curr.next
+        }
+        return result
+    }
+}
+
+extension LinkedList: CustomStringConvertible {
+    public var description: String {
+        var s = "["
+        var node = head
+        while let curr = node {
+            s += "\(curr.value)"
+            node = curr.next
+            if node != nil {
+                s += ", "
+            }
+        }
+        s += "]"
+        return s
+    }
+}
+
+// Initialization from an Array
+extension LinkedList {
+    convenience init(array: Array<T>) {
+        self.init()
+        array.forEach {
+            append($0)
+        }
+    }
+}
+
+// Initialization from an Array literal
+extension LinkedList: ExpressibleByArrayLiteral {
+    public convenience init(arrayLiteral elements: T...) {
+        self.init()
+        elements.forEach {
+            append($0)
+        }
+    }
+}
+
+// Collection
+extension LinkedList: Collection {
+    public typealias Index = LinkedListIndex<T>
+    
+    public var startIndex: Index {
+        get {
+            return LinkedListIndex<T>(node:head, tag:0)
+        }
+    }
+    public var endIndex: Index {
+        get {
+            return LinkedListIndex<T>(node:tail, tag:count)
+        }
+    }
+    public subscript(position: Index) -> T {
+        get {
+            return position.node!.value
+        }
+    }
+    public func index(after idx: Index) -> Index {
+        return LinkedListIndex<T>(node: idx.node?.next, tag: idx.tag + 1)
+    }
+}
+
+public struct LinkedListIndex<T>: Comparable {
+    fileprivate let node: LinkedList<T>.LinkedListNode<T>?
+    fileprivate let tag: Int
+    
+    public static func < <K>(lhs: LinkedListIndex<K>, rhs: LinkedListIndex<K>) -> Bool {
+        return (lhs.tag < rhs.tag)
+    }
+    public static func == <K>(lhs: LinkedListIndex<K>, rhs: LinkedListIndex<K>) -> Bool {
+        return (lhs.tag == rhs.tag)
     }
 }
